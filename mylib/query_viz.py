c8c1e2993d7b5603b46eb21d5c1fab57919df7b3
AliciaXia222 <143651934+AliciaXia222@users.noreply.github.com>
2023-11-25 04:50:02 +0000

    update dataset

--- mylib/query_viz.py
+++ mylib/query_viz.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 def query_transform():
     spark = SparkSession.builder.appName("Query").getOrCreate()
     query = """
-            SELECT India, China, United States
-            FROM population_delta
-            ORDER BY Year DESC
+            SELECT p1.Region, p1.Country Name, p1.2015, p1.2016, p1.2017, p1.2018, p2.2019, p2.2020, p2.2021, p2.2022, AVG(p2.2022) OVER(PARTITION BY p1.Region) as avg_2022
+            FROM popu_1_delta p1
+            JOIN popu_2_delta p2 ON p1.Country Name = p2.Country Name
+            ORDER BY avg_2022 DESC, p1.Country Name
         """
     query_result = spark.sql(query)
     return query_result
@@ -23,56 +24,56 @@ def viz_1():
     else:
         print("No data available. Please double check.")
 
-    query_result = query.select("Region", "avg_year_2022").toPandas()
+    query_result = query.select("Region", "avg_2022").toPandas()
 
     # Group by 'Region' and calculate the mean of 'avg_year_2022'
-    grouped_data = query_result.groupby("Region")["avg_year_2022"].mean().reset_index()
+    grouped_data = query_result.groupby("Region")["avg_2022"].mean().reset_index()
 
     # Create a bar chart with the grouped data
-    grouped_data.plot(kind="bar", x="Region", y="avg_year_2022", color="cyan")
+    grouped_data.plot(kind="bar", x="Region", y="avg_2022", color="cyan")
 
     # Set labels and title
     plt.xlabel("Region")
-    plt.ylabel("Average Wages in 2022")
-    plt.title("Average Wages in 2022 by Region")
+    plt.ylabel("Average Population in 2022")
+    plt.title("Average Population in 2022 by Region")
 
     # Show the plot
     plt.show("Avg_2022_Region.png")
 
-    query_result = query.select("Region", "avg_year_2022").toPandas()
+    query_result = query.select("Region", "avg_2022").toPandas()
 
 
 def viz_2():
     spark = SparkSession.builder.appName("Query").getOrCreate()
     # Write a SQL query to select rows where 'Country' is 'United States' or 'China'
     query = """
-    SELECT country, year_2000, year_2010, year_2020 
-    FROM wages_1_delta 
-    WHERE Country IN ('United States', 'United Kingdom', 'Japan')
+    SELECT Country Name, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 
+    FROM popu_1_delta
+    WHERE Country IN ('United States', 'China', 'Japan')
     """
 
     # Execute the SQL query
     query_result = spark.sql(query)
 
     # Convert the result to a Pandas DataFrame for further operations or visualization
-    wages_us_china = query_result.toPandas().set_index("country")
+    popu_us_china = query_result.toPandas().set_index("Country Name")
 
     # Transposing the DataFrame to have years as the X-axis and wages as the Y-axis
-    wages_transposed = wages_us_china.transpose()
+    popu_transposed = popu_us_china.transpose()
 
     # Plotting the line graph
-    wages_transposed.plot(kind="line", marker="o")
+    popu_transposed.plot(kind="line", marker="o")
 
     # Setting labels and title
     plt.xlabel("Year")
-    plt.ylabel("Wages")
-    plt.title("Development of Wages Over the Years for the US, UK and Japan")
+    plt.ylabel("Population")
+    plt.title("Development of Population Over the Years for the US, China and Japan")
 
     # Display the legend
     plt.legend(title="Country")
 
     # Show the plot
-    plt.show("wages_us_china")
+    plt.show("popu_us_china")
 
 
 if __name__ == "__main__":
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -3,25 +3,25 @@ from pyspark.sql.functions import monotonically_increasing_id
 
 
 def load(
-    dataset="dbfs:/FileStore/Jaxon-Yue-Individual-Project-3/wages_1.csv",
-    dataset2="dbfs:/FileStore/Jaxon-Yue-Individual-Project-3/wages_2.csv",
+    dataset="dbfs:/FileStore/Alicia-Xia-Individual-Project-3/Population_2015-2018.csv",
+    dataset2="dbfs:/FileStore/Alicia-Xia-Individual-Project-3/Population_2019-2022.csv",
 ):
     spark = SparkSession.builder.appName("Read CSV").getOrCreate()
     # load csv and transform it by inferring schema
-    wages_1_df = spark.read.csv(dataset, header=True, inferSchema=True)
-    wages_2_df = spark.read.csv(dataset2, header=True, inferSchema=True)
+    popu_1_df = spark.read.csv(dataset, header=True, inferSchema=True)
+    popu_2_df = spark.read.csv(dataset2, header=True, inferSchema=True)
 
     # add unique IDs to the DataFrames
-    wages_1_df = wages_1_df.withColumn("id", monotonically_increasing_id())
-    wages_2_df = wages_2_df.withColumn("id", monotonically_increasing_id())
+    popu_1_df = popu_1_df.withColumn("id", monotonically_increasing_id())
+    popu_2_df = popu_2_df.withColumn("id", monotonically_increasing_id())
 
     # transform into a delta lakes table and store it
-    wages_1_df.write.format("delta").mode("overwrite").saveAsTable("wages_1_delta")
-    wages_2_df.write.format("delta").mode("overwrite").saveAsTable("wages_2_delta")
+    popu_1_df.write.format("delta").mode("overwrite").saveAsTable("popu_1_delta")
+    popu_2_df.write.format("delta").mode("overwrite").saveAsTable("popu_2_delta")
 
-    num_rows = wages_1_df.count()
+    num_rows = popu_1_df.count()
     print(num_rows)
-    num_rows = wages_2_df.count()
+    num_rows = popu_2_df.count()
     print(num_rows)
 
     return "Completed transform and load"
